db0fe474ae37864827a0d25fa179eefbd64235fc
Dirceusljr <dirceusljr@gmail.com>
2024-08-14 17:51:29 -0300

    feat: :sparkles: add: creating routes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import HeaderButton from './HeaderButton'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Header = () => {
+
   return (
     <header className='w-full h-auto pt-12 px-20 flex justify-end items-center gap-8 pb-10'>
-        <p className='text-white text-lg cursor-pointer hover:underline'>
-            Entrar
-        </p>
-        <HeaderButton>
-            Cadastre-se
-        </HeaderButton>
+        <Link to='/login'>
+          <p className='text-white text-lg cursor-pointer hover:underline'>
+              Entrar
+          </p>
+        </Link>
+        <Link to='/cadastro'>
+          <HeaderButton>
+              Cadastre-se
+          </HeaderButton>
+        </Link>
     </header>
   )
 }
