dc88ffe5773a7626f8e93cff67f54790a5a1330c
Dirceusljr <dirceusljr@gmail.com>
2024-08-15 11:54:35 -0300

    feat: :sparkles: add: login system

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import Example from "./components/Example";
+import PageRouter from "./routes"
 
 const App = () => {
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center bg-gradient-to-r from-gray-700 to-purple-700">
-      <h1 className="text-7xl font-bold mb-5">Equipe LibraryTech</h1>
-      <Example />
-    </div>
+    <PageRouter />
   );
 };
 
