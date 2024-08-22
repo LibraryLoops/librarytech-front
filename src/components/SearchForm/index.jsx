c775294178a55037593abafb7f76e527c7e5e553
heloisacativo <heloisacativo@gmail.com>
2024-08-22 18:34:07 -0300

    Add functionality for searching books

--- src/components/SearchForm/index.jsx
+++ src/components/SearchForm/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const SearchForm = () => {
+const SearchForm = ({value, onChange}) => {
     const nameFiter = "{props}";
     return (
         <form className='m-5'>
@@ -8,6 +8,8 @@ const SearchForm = () => {
                 className='rounded-lg font-sans text-xl w-screen h-18 p-2 font-poppins text-sky-100 bg-stone-700 px-4 max-w-5xl outline-none'
                 type="text"
                 placeholder='Procure por títulos, autor...'
+                value={value}
+                onChange={onChange}
             />
         </form>
     )
