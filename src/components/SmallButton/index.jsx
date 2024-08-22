0b39b0e34d494c0febac945bd6720cae60f8a9c3
heloisacativo <heloisacativo@gmail.com>
2024-08-22 18:36:13 -0300

    Add functionality

--- src/components/SmallButton/index.jsx
+++ src/components/SmallButton/index.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 const SmallButton = ({
   children,
-  color = "bg-stone-700 hover:bg-stone-800",
+  color = "bg-sky-700 hover:bg-stone-800",
+  textColor = 'text-white',
   icon = '',
   onClick
 }) => {
   return (
-    <button className={`px-4 py-2  text-white rounded ${color} flex gap-2`} onClick={onClick}>
+    <button className={`px-4 py-2  ${textColor} rounded ${color} flex gap-2`} onClick={onClick}>
       {icon}
       {children}
     </button>
